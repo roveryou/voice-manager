d102503f6fafe99e9207a6715737621bec748bed
roveryou <roveryou8@gmail.com>
2026-05-11 20:36:38 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Deine Discord User ID (nur DU hast Zugriff)
+OWNER_ID =1481689477851385959  # ← HIER DEINE DISCORD-ID EINTRAGEN
+
+# Bot-Token (aus dem Discord Developer Portal)
+# WICHTIG: Erstelle einen NEUEN Token, der alte ist kompromittiert!
+BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.GeMB5S.b-bL1_360bXvge9HQVi_l2iJSjsL-tKHt4K2ds"
