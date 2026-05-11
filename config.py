16e7d8de919a407a2ffef1dd410aec8e976257dc
roveryou <roveryou8@gmail.com>
2026-05-11 20:39:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ OWNER_ID =1481689477851385959  # ← HIER DEINE DISCORD-ID EINTRAGEN
 
 # Bot-Token (aus dem Discord Developer Portal)
 # WICHTIG: Erstelle einen NEUEN Token, der alte ist kompromittiert!
-BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.GeMB5S.b-bL1_360bXvge9HQVi_l2iJSjsL-tKHt4K2ds"
+BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.G6Re2r.fO8Ry779rHp3tLLgmhBuv11qbet7VkqoWX5pKg"
